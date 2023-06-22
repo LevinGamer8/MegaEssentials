07397f22d2986271938501b48f95281d6e5f2256
Levin <levingamer8@gmail.com>
2023-06-22 19:09:56 +0200

    BANK

--- src/main/java/de/megaessentialsrecode/listeners/Listener.java
+++ src/main/java/de/megaessentialsrecode/listeners/Listener.java
@@ -1,12 +1,17 @@
 package de.megaessentialsrecode.listeners;
 
+import de.megaessentialsrecode.MegaEssentials;
+import de.megaessentialsrecode.scoreboard.Teams;
 import de.megaessentialsrecode.utils.DataBase;
 import de.megaessentialsrecode.utils.Locations;
+import de.megaessentialsrecode.utils.Ränge;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scheduler.BukkitTask;
 
 public class Listener implements org.bukkit.event.Listener {
 
@@ -16,13 +21,24 @@ public class Listener implements org.bukkit.event.Listener {
         if (!(DataBase.exist(p))) {
             DataBase.setup(p);
         }
+        p.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
+        Teams.create(p.getScoreboard());
+        BukkitTask bukkitTask = Bukkit.getScheduler().runTaskTimer(MegaEssentials.getInstance(), () -> Teams.set(p.getScoreboard()), 1, 10);
+        Ränge.putInTeamSchedulers(p, bukkitTask);
         e.setJoinMessage("");
         Locations.teleportToSpawn(p);
     }
 
     @EventHandler
     public void onLeave(PlayerQuitEvent e) {
+        Player p = e.getPlayer();
         e.setQuitMessage("");
+
+        if (Ränge.getTeamScheduler().containsKey(p)) {
+            Ränge.getTeamScheduler().get(p).cancel();
+            Ränge.getTeamScheduler().remove(p);
+        }
+
     }
 
     @EventHandler
